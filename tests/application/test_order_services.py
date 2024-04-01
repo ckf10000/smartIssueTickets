44652365935b984f85fb73a2895836ff66bd4b66
ckf10000 <ckf10000@sina.com>
2024-04-01 10:17:17 +0800

    first

--- tests/application/test_order_services.py
+++ tests/application/test_order_services.py
@@ -12,9 +12,17 @@
 from apps.application.services.order_services import PhoneOrderService
 
 
-if __name__ == "__main__":
+def test_booking_ctrip_special_flight_ticket():
     PhoneOrderService.booking_ctrip_special_flight_ticket(
-            departure_city="CGQ", arrive_city="XUZ", departure_time="2024-04-04 19:35", ac="长龙航空", flight="GJ8060",
-            lowest_price=495.00, passenger="刘铁", age_stage="成人", card_id="220221199005106811", phone="18569520328", 
-            payment_pass="901127"
-        )
+        departure_city="CGQ",
+        arrive_city="XUZ",
+        departure_time="2024-04-04 19:35",
+        ac="长龙航空",
+        flight="GJ8060",
+        lowest_price=495.00,
+        passenger="刘铁",
+        age_stage="成人",
+        card_id="220221199005106811",
+        phone="18569520328",
+        payment_pass="901127",
+    )
